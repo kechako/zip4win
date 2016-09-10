f1a7d2ffe969a5a3532bb669c3a5fc13ac304c17
Tomato Ketchup <r@554.jp>
2016-09-10 15:58:56 +0900

    Move functions to create a zip entory to zip4win package

--- cmd/zip4win/main.go
+++ cmd/zip4win/main.go
@@ -3,72 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
 
 	"github.com/kechako/zip4win"
-	"github.com/pkg/errors"
 )
-
-// writeFile add a new entry to zip archive.
-func writeEntry(w *zip4win.Writer, path string) error {
-	wd, err := os.Getwd()
-	if err != nil {
-		return errors.Wrap(err, "Cound not get the working directory.")
-	}
-	fiWd, err := os.Lstat(wd)
-	if err != nil {
-		return errors.Wrap(err, "Cound not get the working directory.")
-	}
-
-	err = filepath.Walk(path, func(p string, fi os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if os.SameFile(fi, fiWd) {
-			return nil
-		}
-
-		return writeFile(w, p, fi)
-	})
-	if err != nil {
-		if pathErr, ok := err.(*os.PathError); ok {
-			return errors.Wrapf(err, "No such file or directory : %s", pathErr.Path)
-		}
-
-		return err
-	}
-
-	return nil
-}
-
-// writeFile creates a entry to the zip archive.
-func writeFile(w *zip4win.Writer, path string, fi os.FileInfo) error {
-	fw, err := w.Create(fi, path)
-	if err != nil {
-		return errors.Wrap(err, "Could not create a new file in zip archive.")
-	}
-
-	fmt.Printf("%s\n", path)
-
-	if fi.IsDir() {
-		return nil
-	}
-
-	fp, err := os.Open(path)
-	if err != nil {
-		return errors.Wrapf(err, "Could not open the file [%s].", path)
-	}
-	defer fp.Close()
-
-	_, err = io.Copy(fw, fp)
-	if err != nil {
-		return errors.Wrap(err, "Could not write to zip archive.")
-	}
-
-	return nil
-}
 
 // printHelp outputs a help message to STDERR.
 func printHelp() {
@@ -103,7 +41,7 @@ func main() {
 	defer w.Close()
 
 	for _, path := range paths {
-		err = writeEntry(w, path)
+		err = w.WriteEntry(path)
 		if err != nil {
 			printError(err)
 		}
--- zip4win.go
+++ zip4win.go
@@ -3,6 +3,7 @@ package zip4win
 import (
 	"archive/zip"
 	"bytes"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -31,7 +32,7 @@ func (w *Writer) Close() error {
 }
 
 // Create adds a file to zip file using the provided name.
-func (w *Writer) Create(fi os.FileInfo, name string) (io.Writer, error) {
+func (w *Writer) create(fi os.FileInfo, name string) (io.Writer, error) {
 	h, err := zip.FileInfoHeader(fi)
 	if err != nil {
 		return nil, err
@@ -57,6 +58,65 @@ func (w *Writer) Create(fi os.FileInfo, name string) (io.Writer, error) {
 	}
 
 	return w.zw.CreateHeader(h)
+}
+
+// writeFile add a new entry to zip archive.
+func (w *Writer) WriteEntry(path string) error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return errors.Wrap(err, "Cound not get the working directory.")
+	}
+	fiWd, err := os.Lstat(wd)
+	if err != nil {
+		return errors.Wrap(err, "Cound not get the working directory.")
+	}
+
+	err = filepath.Walk(path, func(p string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if os.SameFile(fi, fiWd) {
+			return nil
+		}
+
+		return w.writeFile(p, fi)
+	})
+	if err != nil {
+		if pathErr, ok := err.(*os.PathError); ok {
+			return errors.Wrapf(err, "No such file or directory : %s", pathErr.Path)
+		}
+
+		return err
+	}
+
+	return nil
+}
+
+// writeFile creates a entry to the zip archive.
+func (w *Writer) writeFile(path string, fi os.FileInfo) error {
+	fw, err := w.create(fi, path)
+	if err != nil {
+		return errors.Wrap(err, "Could not create a new file in zip archive.")
+	}
+
+	fmt.Printf("%s\n", path)
+
+	if fi.IsDir() {
+		return nil
+	}
+
+	fp, err := os.Open(path)
+	if err != nil {
+		return errors.Wrapf(err, "Could not open the file [%s].", path)
+	}
+	defer fp.Close()
+
+	_, err = io.Copy(fw, fp)
+	if err != nil {
+		return errors.Wrap(err, "Could not write to zip archive.")
+	}
+
+	return nil
 }
 
 // convertToShiftJIS converts a UTF-8 string to a ShiftJIS string.
